f749acb434cd0cb50910c103d838cc5213c6999a
titus-cloud <titus.onzere@student.moringaschool.com>
2024-07-13 23:31:01 +0300

    event acting on events

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.left = '180px'; 
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left.replace('px', ''));
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left.replace('px', ''));
+  if (left < 360) { 
+    dodger.style.left = `${left + 10}px`;
+  }
+}
+
+function handleKeyDown(event) {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+}
+
+
